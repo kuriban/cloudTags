785124907075e95324c8a1c67f9db77456309742
Igor Kuripko <igor.kuripko@andersenlab.com>
2019-01-24 23:57:56 +0200

    change styles, behavior

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,22 +3,58 @@ import propTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router";
 
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  display: inline-block;
+  position: absolute;
+  left: 30%;
+  top: 30%;
+  width: 600px;
+`;
+
+const StyledBlock = styled.div`
+  display: inline-block;
+  align-items: flex-start
+  margin: 5px;
+  transform: ${props => props.isRotate && "rotate(90deg)"}
+`;
+
+const MAX_FONT_SIZE = 100;
+
+const findMaxSentimentScore = initialData => {
+  const arrSentimentScores = [];
+
+  Object.keys(initialData).forEach(el => {
+    arrSentimentScores.push(initialData[el].sentimentScore);
+  });
+
+  return Math.max.apply(null, [...arrSentimentScores]);
+};
+
 class App extends Component {
   render() {
     const { initialData } = this.props;
+    const maxSentimentScore = findMaxSentimentScore(initialData);
 
     return (
-      <div>
+      <StyledContainer>
         {Object.keys(initialData).map(id => {
           const { sentimentScore, label } = initialData[id];
+          const isRotate = !!((Math.random() * 10).toFixed(0) % 2);
+          const style = {
+            fontSize: (sentimentScore * MAX_FONT_SIZE) / maxSentimentScore
+          };
 
           return (
-            <Link to={`/${id.replace("/", "**")}`} key={id} title={label}>
-              <span style={{ fontSize: sentimentScore }}>{label}</span>
-            </Link>
+            <StyledBlock isRotate={isRotate} key={id}>
+              <Link to={`/${id.replace("/", "**")}`} key={id} title={label}>
+                <span style={style}>{label}</span>
+              </Link>
+            </StyledBlock>
           );
         })}
-      </div>
+      </StyledContainer>
     );
   }
 }
